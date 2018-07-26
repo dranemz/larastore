e0738d4744493be453bd8c5b9407aab7755fe664
Your Name <you@example.com>
2018-07-26 12:13:12 +0800

    new update

--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 //all Products
 Route::get('product', 'ProductController@index');
-
 //single product
 Route::get('product/{id}',"ProductController@show");
-
 //create product
 Route::post('product','ProductController@store');
 //update product
 Route::put('product','ProductController@store');
-
 //delete product
 Route::delete('product/{id}','ProductController@destroy');
+
+//all Category
+Route::get('category', 'CategoryController@index');
+//single Category
+Route::get('category/{id}',"CategoryController@show");
+//create Category
+Route::post('category','CategoryController@store');
+//update Category
+Route::put('category','CategoryController@store');
+//delete Category
+Route::delete('category/{id}','CategoryController@destroy');
